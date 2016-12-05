0a75185ad5cd216f4b3b180271522446f71ef261
Dima <Dima@MacBook-Pro-mac.local>
2016-12-06 00:58:47 +0200

    added validations

--- js/validations.js
+++ js/validations.js
@@ -5,13 +5,13 @@ $(document).ready(function () {
     $("#form1").validate({
         rules: {
             display_name:{
-
+                minlength: 4
             },
             user_name:{
-
+                minlength: 4
             },
             password:{
-
+                minlength: 8
             },
             confirm_password:{
                 equalTo: "#password"
@@ -19,8 +19,13 @@ $(document).ready(function () {
 
         },
         messages: {
-            email: {
-                required: "We need your email address to contact you",
+            display_name: {
+                minlength: jQuery.validator.format("At least {0} characters required!")
+            },
+            user_name: {
+                minlength: jQuery.validator.format("At least {0} characters required!")
+            },
+            password: {
                 minlength: jQuery.validator.format("At least {0} characters required!")
             }
         }
@@ -31,40 +36,62 @@ $(document).ready(function () {
     $("#form2").validate({
         rules: {
             company_name:{
-
+                minlength: 2
             },
             email: {
                 required: true,
                 email: true
             },
             title:{
-
+                minlength: 4
             },
             first_name: {
                 required: true,
-                minlength: 2
+                minlength: 3
             },
             middle_name:{
-
+                minlength: 3
             },
             last_name:{
+                minlength: 3,
                 required: true
             },
             address_1:{
+                minlength: 3,
                 required: true
             },
             address_2:{
-
+                minlength: 3
             }
 
         },
         messages: {
+            company_name: {
+                minlength: jQuery.validator.format("At least {0} characters required!")
+            },
             email: {
                 required: "We need your email address to contact you",
                 minlength: jQuery.validator.format("At least {0} characters required!")
             },
+            title: {
+                minlength: jQuery.validator.format("At least {0} characters required!")
+            },
             first_name: {
                 required: "We need your first name to contact you",
+                minlength: jQuery.validator.format("At least {0} characters required!")
+            },
+            middle_name: {
+                minlength: jQuery.validator.format("At least {0} characters required!")
+            },
+            last_name: {
+                required: "We need your last name to contact you",
+                minlength: jQuery.validator.format("At least {0} characters required!")
+            },
+            address_1: {
+                required: "We need your address to contact you",
+                minlength: jQuery.validator.format("At least {0} characters required!")
+            },
+            address_2: {
                 minlength: jQuery.validator.format("At least {0} characters required!")
             }
         }
@@ -80,9 +107,18 @@ $(document).ready(function () {
             phone_number.match(filter);
     }, "Please specify a valid phone number");
 
+    jQuery.validator.addMethod("zipUS", function(zip, element) {
+        zip = zip.replace(/\s+/g, "");
+        var filter =  /^\d{5}(-\d{4})?(?!-)$/;
+        alert(zip.match(filter));
+        return this.optional(element) && zip.match(filter);
+    }, "Please specify a valid zip code");
+
+
     $("#form3").validate({
         rules: {
             zip:{
+                zipUS: true,
                 required: true
             },
             confirm_password2:{
